5709691e031ecb54d5c276f20b2f52b99cb91d13
Marc IceMaD Duboc <marc.duboc@smartangels.fr>
2016-02-23 11:21:59 +0100

    fix norm

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    var ConfigBuild = {
+        config: {},
+
+        addSass: function(name) {
+            this.config.sass = this.config.sass || {};
+
+            var config = {
+                files: {}
+            };
+
+            config.files['dist/css/' + name + '.css'] = 'src/sass/' + name + '.scss';
+
+            this.config.sass[name] = config;
+
+            return this;
+        },
+
+        addWatch: function(directory, name) {
+            this.config.watch = this.config.watch || {};
+
+            this.config.watch[name] = {
+                files: 'src/' + directory + '/**/*.scss',
+                tasks: [directory + ':' + name],
+                options: {
+                    interrupt: true,
+                    livereload: true
+                }
+            }
+        }
+    };
+
+    ConfigBuild.addSass('style');
+    ConfigBuild.addWatch('sass', 'style');
+
+    grunt.initConfig(ConfigBuild.config);
+
+    grunt.registerTask('default', ['watch']);
+};
